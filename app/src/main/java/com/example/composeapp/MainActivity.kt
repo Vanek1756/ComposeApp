8ad3d34f5d6de559823c7c8384b9d36d6ffc6745
babiychuk.vania <babiychuk.vania@gmail.com>
2022-12-13 15:04:49 +0200

    Created composable function TimesTable.

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,20 +3,53 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeapp.ui.theme.ComposeAppTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            TimesTable()
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val color = if ((i + j) % 2 == 0) Color.Yellow else Color.White
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.DarkGray)
+                            .background(color),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${i * j}")
+                    }
+                }
+            }
         }
     }
 }
